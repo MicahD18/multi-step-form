a9eebd968884add83213c4785e49fe332fea7f43
MicahD18 <micah.daise@nwktc.edu>
2022-12-20 17:59:26 -0700

    the UI for step 3 of the form is complete

--- src/app/components/addon/addon.component.ts
+++ src/app/components/addon/addon.component.ts
@@ -1,18 +1,38 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { AddOn } from 'src/app/models/add-on.model';
 
 @Component({
   selector: 'app-addon',
   templateUrl: './addon.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AddonComponent implements OnInit {
   @Output() step = new EventEmitter<number>();
 
-  constructor() { }
+  addOns: AddOn[] = [
+    {
+      id: 1,
+      name: 'Online service',
+      description: 'Access to multiplayer games',
+      price: '+$1/mo',
+    },
+    {
+      id: 2,
+      name: 'Larger storage',
+      description: 'Extra 1TB of cloud save',
+      price: '+$2/mo',
+    },
+    {
+      id: 3,
+      name: 'Customizable Profile',
+      description: 'Custom theme on your profile',
+      price: '+$2/mo',
+    },
+  ];
 
-  ngOnInit(): void {
-  }
+  constructor() {}
+
+  ngOnInit(): void {}
 
   onPrevStep(prevStep: number): void {
     this.step.emit(prevStep);
@@ -21,5 +41,4 @@ export class AddonComponent implements OnInit {
   onNextStep(nextStep: number): void {
     this.step.emit(nextStep);
   }
-
 }
---
+++ src/app/models/add-on.model.ts
@@ -0,0 +1,6 @@
+export interface AddOn {
+    id: number;
+    name: string;
+    description: string;
+    price: string;
+}
